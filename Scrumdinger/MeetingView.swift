eb81b0c8874be0be3ee4caef4afc617bfdaa7ea2
Rafhael Payao <rafeltmbr@gmail.com>
2023-11-20 12:39:10 -0300

    feat: created the static layout for the Meetings view

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 300, total: 900)
+                .padding(.bottom)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
