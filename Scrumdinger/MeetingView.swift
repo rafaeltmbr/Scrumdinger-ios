b36d1d0ab91f4d60a9f2d5138a8daf3982cf48d5
Rafhael Payao <rafaeltmbr@gmail.com>
2024-03-23 16:13:23 -0300

    feat: finished chapter 1. Created basic scrum view

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total: 100)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle().strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button("", systemImage: "forward.fill") { }.accessibilityLabel("Next speaker")
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
