903e4eeb504f8a77beee2af412d8c9c2e4082c9c
Rafhael Payao <rafaeltmbr@gmail.com>
2024-03-23 13:49:07 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rafhael Payão on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
